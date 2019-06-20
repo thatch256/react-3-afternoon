8114e92780d981e71ec3690a341edfb0babbe52f
thatch256 <thatch256@gmail.com>
2019-06-20 15:16:39 -0600

    Finished project

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,10 @@
 import React, { Component } from 'react';
-
+import axios from 'axios'
 import './App.css';
+import { ToastContainer, toast } from 'react-toastify';
+import { totalmem } from 'os';
+import { join } from 'path';
+import Post from './Post/Post'
 
 import Header from './Header/Header';
 import Compose from './Compose/Compose';
@@ -19,19 +23,39 @@ class App extends Component {
   }
   
   componentDidMount() {
-
+    axios.get('https://practiceapi.devmountain.com/api/posts')
+      .then(res => {
+        this.setState({
+          posts: res.data
+        })
+      })
   }
 
-  updatePost() {
-  
+  updatePost(id, text) {
+    axios.put(`https://practiceapi.devmountain.com/api/posts?id=${id}`, {text})
+      .then( res => {
+        this.setState({
+          posts: res.data
+        })
+      })
   }
 
-  deletePost() {
-
+  deletePost(id) {
+    axios.delete(`https://practiceapi.devmountain.com/api/posts?id=${id}`)
+    .then( res => {
+      this.setState({
+        posts: res.data
+      })
+    })
   }
 
-  createPost() {
-
+  createPost(text) {
+    axios.post(`https://practiceapi.devmountain.com/api/posts`, {text})
+    .then( res => {
+      this.setState({
+        posts: res.data
+      })
+    })
   }
 
   render() {
@@ -43,8 +67,19 @@ class App extends Component {
 
         <section className="App__content">
 
-          <Compose />
+          <Compose createPostFn={this.createPost}/>
           
+          {
+            posts.map(post => (
+              <Post key={post.id} 
+                    text={post.text} 
+                    date={post.date} 
+                    updatePostFn={this.updatePost} 
+                    id={post.id}
+                    deletePostFn={this.deletePost}/>
+            ))
+          }
+
         </section>
       </div>
     );
